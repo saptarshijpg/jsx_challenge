c508942c7a7ae9943b11a5a335dc4b4a17b9d1e1
rikjal <saptarshi.jpg@gmail.com>
2020-10-01 16:45:33 +0530

    Update Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,3 +1,3 @@
 import React from 'react'
 
-export default Error = () => (<h1>Oops! Error:404! Page Not Found</h1>)
+export default Error = () => (<h1 style={{ color: "red" }}>Oops! Error: 404! Page Not Found!!</h1>)
